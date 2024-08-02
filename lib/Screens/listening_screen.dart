2363479c4673c83a49c9b83bc98805aae3aff0c6
NAV <navjotnabha@gmail.com>
2024-08-02 16:22:55 +1000

    all well 2 before json change , question.json is now data.json. adding narration inside data.json. these changes will be in next commit. at the moment all is well

--- lib/Screens/listening_screen.dart
+++ lib/Screens/listening_screen.dart
@@ -15,6 +15,8 @@ class _ListeningScreenState extends State<ListeningScreen> {
   bool _isPlaying = false;
   bool _isAudioComplete = false;
   Map<String, dynamic> _questionsData = {}; // Initialize with an empty map
+  String? _imageUrl; // Make _imageUrl nullable
+  Map<int, String> _draggedAnswers = {};
 
   @override
   void initState() {
@@ -29,19 +31,27 @@ class _ListeningScreenState extends State<ListeningScreen> {
     setState(() {
       _isLoading = true;
       _isAudioComplete = false;
+      _imageUrl = null;
+      _draggedAnswers.clear();
     });
 
     final questionsUrl = "https://firebasestorage.googleapis.com/v0/b/ielts-gym-edbdf.appspot.com/o/listening_material%2Ftest_1%2Fsection$_currentSection%2Fquestions.json?alt=media&token=5360fb8b-f8b7-4b81-85d4-f846a3aa098f";
     final audioUrlMp3 = "https://firebasestorage.googleapis.com/v0/b/ielts-gym-edbdf.appspot.com/o/listening_material%2Ftest_1%2Fsection$_currentSection%2Faudio.mp3?alt=media&token=5360fb8b-f8b7-4b81-85d4-f846a3aa098f";
+    final imageUrl = "https://firebasestorage.googleapis.com/v0/b/ielts-gym-edbdf.appspot.com/o/listening_material%2Ftest_1%2Fsection$_currentSection%2Fimage.jpg?alt=media&token=5360fb8b-f8b7-4b81-85d4-f846a3aa098f";
 
     try {
       final questionsResponse = await http.get(Uri.parse(questionsUrl));
       if (questionsResponse.statusCode == 200) {
         _questionsData = json.decode(questionsResponse.body);
-        if (_questionsData['questions'] == null) {
+        if (_questionsData['questions'] == null && _questionsData['questionType'] != 'fillMap') {
           throw Exception('No questions found for section $_currentSection');
         }
         print('Loaded section $_currentSection');
+
+        if (_questionsData['questionType'] == 'fillMap') {
+          _imageUrl = imageUrl;
+        }
+
         setState(() {
           _isLoading = false;
         });
@@ -88,7 +98,10 @@ class _ListeningScreenState extends State<ListeningScreen> {
     });
   }
 
-  void _nextSection() {
+  void _nextSection() async {
+    print('Stopping current audio');
+    await _audioPlayer.stop();  // Stop any currently playing audio
+
     print('Loading next section');
     setState(() {
       _currentSection++;
@@ -108,17 +121,36 @@ class _ListeningScreenState extends State<ListeningScreen> {
         children: [
           _isLoading
               ? Center(child: CircularProgressIndicator())
-              : _questionsData.isEmpty
-              ? Center(child: Text('No data found'))
-              : ListView.builder(
-            itemCount: _questionsData['questions'].length,
-            itemBuilder: (context, index) {
-              final question = _questionsData['questions'][index];
-              return ListTile(
-                title: Text(question['question']),
-                subtitle: Text('Answer: ${question['answer']}'),
-              );
-            },
+              : Column(
+            children: [
+              if (_imageUrl != null)
+                Expanded(
+                  flex: 3,
+                  child: Image.network(
+                    _imageUrl!,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              if (_questionsData['questionType'] == 'fillMap')
+                Expanded(
+                  flex: 2,
+                  child: _buildFillMapQuestions(),
+                ),
+              if (_questionsData['questionType'] != 'fillMap')
+                Expanded(
+                  flex: 5,
+                  child: ListView.builder(
+                    itemCount: _questionsData['questions']?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      final question = _questionsData['questions'][index];
+                      return ListTile(
+                        title: Text(question['question']),
+                        subtitle: Text('Answer: ${question['answer']}'),
+                      );
+                    },
+                  ),
+                ),
+            ],
           ),
           Positioned(
             bottom: 16,
@@ -136,9 +168,96 @@ class _ListeningScreenState extends State<ListeningScreen> {
     );
   }
 
-  @override
-  void dispose() {
-    _audioPlayer.dispose();
-    super.dispose();
+  Widget _buildFillMapQuestions() {
+    final blanks = _questionsData['blanks'] as List<dynamic>;
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: blanks.length,
+            itemBuilder: (context, index) {
+              final blank = blanks[index];
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${blank['id']}. ${blank['correctAnswer']}',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(width: 10),
+                    DragTarget<String>(
+                      onAccept: (receivedItem) {
+                        setState(() {
+                          _draggedAnswers[blank['id']] = receivedItem;
+                        });
+                      },
+                      builder: (context, acceptedItems, rejectedItems) {
+                        return Container(
+                          width: 100,
+                          height: 40,
+                          color: Colors.grey[200],
+                          child: Center(
+                            child: Text(
+                              _draggedAnswers[blank['id']] ?? blank['placeholder'],
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+        Wrap(
+          spacing: 10.0,
+          runSpacing: 10.0,
+          children: blanks.map((blank) {
+            return Draggable<String>(
+              data: blank['label'],
+              feedback: Material(
+                child: Container(
+                  width: 50,
+                  height: 40,
+                  color: Colors.blue,
+                  child: Center(
+                    child: Text(
+                      blank['label'],
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ),
+                ),
+              ),
+              childWhenDragging: Container(
+                width: 50,
+                height: 40,
+                color: Colors.grey,
+                child: Center(
+                  child: Text(
+                    blank['label'],
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              ),
+              child: Container(
+                width: 50,
+                height: 40,
+                color: Colors.green,
+                child: Center(
+                  child: Text(
+                    blank['label'],
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      ],
+    );
   }
 }
